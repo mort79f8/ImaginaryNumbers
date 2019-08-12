dc5b92a1cecd6cfa36b5774360a2e3d9481e9848
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-08-12 11:28:00 +0200

    Lists created, started on creating names.

--- NameGenerator/Program.cs
+++ NameGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NameGenerator
 {
@@ -6,7 +7,26 @@ namespace NameGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> preLettersMale = new List<string>() {"S","Sp","Sk","St","T"};
+            List<string> vocals = new List<string>() {"a","e","i","o","u","y" };
+            List<string> middleLettersMale = new List<string>() {"r","t","p","d","f","j","k","l","v","b","n","m" };
+            List<string> endLettersMale = new List<string>() {"q","p","k","ck","l" };
+            string femaleStart = "T'";
+            List<string> preLettersFemale = new List<string>() { "P", "K", "Q" };
+            List<string> endLettersFemale = new List<string>() { "r", "j", "'p", "k", "l" };
+            List<string> namesMale = new List<string>();
+            List<string> namesFemale = new List<string>();
+
+            foreach (var letter in preLettersMale)
+            {
+                string name = "";
+                name = letter;
+                
+
+                Console.WriteLine(name);
+                Console.WriteLine();            
+            }
+
         }
     }
 }
